e52f8efdac7ca94ed2b586c7c08fa4e8fdcae240
jimmyliuzg <jimmyliuzg@gmail.com>
2026-01-20 08:51:55 -0800

    chore: update dependencies and regenerate package-lock.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [
     tailwind({
       configFile: './tailwind.config.cjs',
-      applyBaseStyles: true // let Tailwind inject base styles
+      applyBaseStyles: true
     }),
     react()
   ],
   output: 'static',
-  site: 'https://yourname.com', // replace later
+  adapter: cloudflare(),
+  site: 'https://congenial-broccoli.pages.dev',
 });
